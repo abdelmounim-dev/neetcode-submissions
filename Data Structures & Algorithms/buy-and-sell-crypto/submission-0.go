bdf07a4c2e8e86b27d416ec473fb12558f91e71b
Abdelmounim Baouia <77006851+abdelmounim-dev@users.noreply.github.com>
2026-06-16 13:48:23 +0100

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.go
@@ -0,0 +1,15 @@
+func maxProfit(prices []int) int {
+	min, max, profit := prices[0], prices[0], 0
+	for _,v := range prices {
+		if v < min {
+			min = v
+			max = v
+			continue
+		}
+		if v > max {
+			max = v
+			profit = max - min
+		}
+	}
+	return profit
+}
